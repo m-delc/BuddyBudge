92800af33c18204281dca87ca491af8b473680ac
Michael Delcambre <delcambrem@gmail.com>
2022-02-23 22:53:20 -0600

    commit before Grid test

--- frontend/src/components/MenuAppBar/MenuAppBar.js
+++ frontend/src/components/MenuAppBar/MenuAppBar.js
@@ -11,10 +11,13 @@ import FormControlLabel from "@mui/material/FormControlLabel";
 import FormGroup from "@mui/material/FormGroup";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
+import { useNavigate, Link } from "react-router-dom";
+
 
 export default function MenuAppBar() {
   const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
+  // const navigate = useNavigate()
 
   const handleChange = (event) => {
     setAuth(event.target.checked);
@@ -37,6 +40,7 @@ export default function MenuAppBar() {
               checked={auth}
               onChange={handleChange}
               aria-label="login switch"
+              // onChange={navigate()}
             />
           }
           label={auth ? "Logout" : "Login"}
@@ -53,6 +57,7 @@ export default function MenuAppBar() {
           >
             <MenuIcon />
           </IconButton>
+          <Link to='/login'>Login</Link>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Photos
           </Typography>
--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,83 +1,91 @@
 import React, { useState } from "react";
-import Box from "@mui/material/Box";
+// import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
-import PublishIcon from '@mui/icons-material/Publish';
+import PublishIcon from "@mui/icons-material/Publish";
+import { Grid } from "@material-ui/core";
+
 const Signup = () => {
   // IF YOU ADD THESE BACK, remember to add the params back in user controller
   //   const [signupFirstName, setSignupFirstName] = useState("");
   //   const [signupLastName, setSignupLastName] = useState("");
 
-  //   const [signupUsername, setSignupUsername] = useState("");
-  //   const [signupPassword, setSignupPassword] = useState("");
-  //   const [passwordConfirmation, setPasswordConfirmation] = useState("");
-  //   const [signupErrors, setSignupErrors] = useState([]);
+  // const [signupUsername, setSignupUsername] = useState("");
+  // const [signupPassword, setSignupPassword] = useState("");
+  // const [passwordConfirmation, setPasswordConfirmation] = useState("");
+  // const [signupErrors, setSignupErrors] = useState([]);
 
-  //   function signupOnSubmit(e) {
-  //     e.preventDefault();
-  //     // setSignupErrors([])
-  //     const user = {
-  //       //   first_name: signupFirstName,
-  //       //   last_name: signupLastName,
-  //       username: signupUsername,
-  //       password: signupPassword,
-  //       password_confirmation: passwordConfirmation,
-  //     };
-  //     fetch("/users", {
-  //       method: "POST",
-  //       headers: { "Content-Type": "application/json" },
-  //       body: JSON.stringify(user),
-  //     }).then((res) => {
-  //       if (res.ok) {
-  //         res.json().then((user) => {
-  //           setUser(user);
-  //           setIsAuthenticated(true);
-  //           navigate("/home");
-  //         });
-  //       } else {
-  //         res.json().then((json) => setSignupErrors(Object.entries(json.error)));
-  //       }
-  //     });
-  //   }
+  // function signupOnSubmit(e) {
+  //   e.preventDefault();
+  //   // setSignupErrors([])
+  //   const user = {
+  //     //   first_name: signupFirstName,
+  //     //   last_name: signupLastName,
+  //     username: signupUsername,
+  //     password: signupPassword,
+  //     password_confirmation: passwordConfirmation,
+  //   };
+  //   fetch("/users", {
+  //     method: "POST",
+  //     headers: { "Content-Type": "application/json" },
+  //     body: JSON.stringify(user),
+  //   }).then((res) => {
+  //     if (res.ok) {
+  //       res.json().then((user) => {
+  //         setUser(user);
+  //         setIsAuthenticated(true);
+  //         navigate("/home");
+  //       });
+  //     } else {
+  //       res.json().then((json) => setSignupErrors(Object.entries(json.error)));
+  //     }
+  //   });
+  // }
 
   return (
-
     <form>
-      <div>
-        <TextField
-          //   required
-          id="standard-required"
-          label="Username"
-          //   defaultValue="Username"
-          variant="standard"
-        />
-      </div>
-
       <br></br>
-
-      <div>
-        <TextField
-          id="standard-password-input"
-          label="Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-      </div>
-
       <br></br>
+      <Grid container>
+          <Grid item xs={6}>
+              
+          </Grid>
+          <Grid item xs={6}></Grid>
+        <div>
+          <TextField
+            //   required
+            id="standard-required"
+            label="Username"
+            //   defaultValue="Username"
+            variant="standard"
+          />
+        </div>
 
-      <div>
-        <TextField
-          id="standard-password-confirm-input"
-          label="Confirm Password"
-          type="password"
-          autoComplete="current-password"
-          variant="standard"
-        />
-      </div>
+        <br></br>
 
-      <br></br>
+        <div>
+          <TextField
+            id="standard-password-input"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+          />
+        </div>
+
+        <br></br>
+
+        <div>
+          <TextField
+            id="standard-password-confirm-input"
+            label="Confirm Password"
+            type="password"
+            autoComplete="current-password"
+            variant="standard"
+          />
+        </div>
+
+        {/* <br></br>
 
       <div>
         <TextField
@@ -89,19 +97,22 @@ const Signup = () => {
           }}
           variant="standard"
         />
-      </div>
+      </div> */}
 
-      <br></br>
+        <br></br>
+        <br></br>
 
-      <div>
-        <Button
-          variant="outlined"
-          endIcon={<PublishIcon />}
-          className="btn-signup"
-        >
-          Submit
-        </Button>
-      </div>
+        <div>
+          <Button
+            variant="outlined"
+            endIcon={<PublishIcon />}
+            className="btn-signup"
+            type="Submit"
+          >
+            Submit
+          </Button>
+        </div>
+      </Grid>
     </form>
   );
 };
