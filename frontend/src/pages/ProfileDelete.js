53facc3abd3614a2d885bf18a6d325db606f2ece
Michael Delcambre <delcambrem@gmail.com>
2022-02-27 23:41:36 -0600

    delete profile redirect working

--- frontend/src/pages/ProfileDelete.js
+++ frontend/src/pages/ProfileDelete.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "../css/profiledelete.css";
 
 const ProfileDelete = ({ user, setUser, setIsAuthenticated }) => {
-  const handleDeleteAccount = (e) => {
+  const [someState, setSomeState] = useState(true);
+  const navigate = useNavigate();
+
+  const x = (e) => {
     e.preventDefault();
+    setSomeState(false);
+    setTimeout(handleDeleteAccount, 3000);
+  };
+
+  const handleDeleteAccount = () => {
     fetch(`/users/${user.id}`, {
       method: "DELETE",
       headers: { "Content-Type": "application/json" },
-    }).then(console.log("Account Deleted"));
+    }).then(
+      console.log("Account Deleted"),
+      setUser(""),
+      setIsAuthenticated(false),
+      navigate("/home")
+    );
   };
 
   return (
-    <div>
-      {user ? user.username : null}, are you sure you want to delete your
-      account?
-      <form onSubmit={handleDeleteAccount}>
-        <br></br>
-        <input required type="checkbox" />
-        Yes, I'm sure
-        <br></br>
-        <button type="submit">DELETE ACCOUNT</button>
-      </form>
-    </div>
+    <>
+      {someState ? (
+        <div>
+          {user ? user.username : null}, are you sure you want to delete your
+          account?
+          <form onSubmit={x}>
+            <br></br>
+            <input required type="checkbox" />
+            Yes, I'm sure
+            <br></br>
+            <button type="submit">DELETE ACCOUNT</button>
+          </form>
+        </div>
+      ) : (
+        <div>We're sorry to see you go. Redirecting...</div>
+      )}
+    </>
   );
 };
 
