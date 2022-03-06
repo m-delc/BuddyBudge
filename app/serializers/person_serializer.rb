7b63513877e398571fe404e98bfcb802d5a79bd4
Michael Delcambre <delcambrem@gmail.com>
2022-03-05 21:53:41 -0600

    personbudget is now serialized to person

--- app/serializers/person_serializer.rb
+++ app/serializers/person_serializer.rb
@@ -3,6 +3,7 @@ class PersonSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :bio, :img, :savings1, :savings2, :savings3, :savings4, :savings5, :savings6, :goal1, :goal2, :goal3, :goal4, :goal5, :goal6
   
   has_many :friends
+  has_one :person_budget
   has_many :users, through: :friends
 
 end
