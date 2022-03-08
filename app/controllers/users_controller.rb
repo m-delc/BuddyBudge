92c0b1b33254910c2bf0dfef840415fdbe16bade
Michael Delcambre <delcambrem@gmail.com>
2022-03-08 16:29:22 -0600

    signup working fully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,7 @@ class UsersController < ApplicationController
     def create
         user = User.create!(user_params)
         if user.valid?
+            session[:current_user] = user.id
             return render json: user, status: 201
         else
             return render json: { error: user.errors.full_messages }, status: 404
