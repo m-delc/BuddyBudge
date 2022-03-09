aeb9a3e0bb92f13f1c88bd08791f9d87af7042b1
Michael Delcambre <delcambrem@gmail.com>
2022-03-09 16:54:41 -0600

    fixed seed for deployment

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,18 @@
 Rails.application.routes.draw do
   
-  # namespace :api do
+  namespace :api do
   
-  resources :users, only: [:update, :create, :destroy, :index, :show]
-  resources :people, only: [:index, :update, :show]
-  resources :friends, only: [:create, :index, :destroy, :show]
-  resources :budgets, only: [:create, :index]
-  resources :person_budgets, only: [:index]
-    
-  post '/login', to: "sessions#login"
-  get '/authorize_user', to: "users#show"
-  delete '/logout', to: "sessions#logout"
+    resources :users, only: [:update, :create, :destroy, :index, :show]
+    resources :people, only: [:index, :update, :show]
+    resources :friends, only: [:create, :index, :destroy, :show]
+    resources :budgets, only: [:create, :index]
+    resources :person_budgets, only: [:index]
+      
+    get '/authorize_user', to: "users#show"
+    post '/login', to: "sessions#login"
+    delete '/logout', to: "sessions#logout"
 
-
-    
-  # end
+  end
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
   
