22885d11fd5196010e62b0c42c31347c1be9f999
Michael Delcambre <delcambrem@gmail.com>
2022-03-10 14:45:08 -0600

    pics local

--- frontend/src/components/People.js
+++ frontend/src/components/People.js
@@ -27,7 +27,7 @@ const People = ({ user, setUser, setIsAuthenticated, userFriends, people }) => {
         textAlign: "center",
       }}
     >
-      <img src="https://www.coredocuments.com/wp-content/uploads/2019/04/happy-people-money-falling.jpg"></img>
+      <img src="/public/img/people.jpg" alt="people-money"></img>
       <br></br>
       <div style={{ fontFamily: "Roboto", fontSize: "20px" }}>
         Make Some Friends!
