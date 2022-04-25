339b71e61c9fc88b0c6aa01aee8df78665c19fca
Michael Delcambre <delcambrem@gmail.com>
2022-04-25 18:50:19 -0500

    patch issue solved thru modifying validations in rails

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
     has_many :friends
     has_many :people, through: :friends
     validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
+    validates :password, presence: true, allow_blank: true
     # validates :password_confirmation, presence: true
     validates :first_name, presence: true
     
