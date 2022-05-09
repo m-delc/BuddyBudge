f35b2b1f63a7fbb8c746ffce5deec85cf59ee8aa
Michael Delcambre <delcambrem@gmail.com>
2022-05-08 23:59:13 -0500

    added a user to seed for convienence. dropped :username from Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_03_205914) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_09_044956) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -54,12 +54,11 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_03_205914) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
+    t.string "email"
     t.string "first_name"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "email"
   end
 
 end
