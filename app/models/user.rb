8e9b5ee0d7e84b7e204c10e0fbae1a02616ff627
Michael Delcambre <delcambrem@gmail.com>
2022-05-08 23:41:49 -0500

    email validations are in place, password_confirmation validation re-added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
     has_one :budget
     has_many :friends
     has_many :people, through: :friends
-    # validates :email, presence: true, uniqueness: true
     validates :email, email: {mode: :strict, require_fqdn: true}
     validates :password, presence: true, allow_blank: true
-    # validates :password_confirmation, presence: true
+    validates :password_confirmation, presence: true
     validates :first_name, presence: true
     
 end
