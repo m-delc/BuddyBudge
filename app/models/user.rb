26725a36d561147c437012eca6544750160c6ff3
Michael Delcambre <delcambrem@gmail.com>
2022-06-01 10:35:46 -0500

    added email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     has_one :budget
     has_many :friends
     has_many :people, through: :friends
-    validates :email, email: {mode: :strict, require_fqdn: true}
+    validates :email, email: {mode: :strict, require_fqdn: true}, uniqueness: true
     validates :password, presence: true, allow_blank: true
     validates :password_confirmation, presence: true, allow_blank: true
     validates :first_name, presence: true
