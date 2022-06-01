398b26dbef78834711b4df4d8ddc76dca43142d8
Michael Delcambre <delcambrem@gmail.com>
2022-06-01 10:44:37 -0500

    removed unnecessary open_timeout/read_timeout from dev and prod ENVs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,9 +74,7 @@ Rails.application.configure do
     user_name:            ENV['gmail_username'],
     password:             ENV['gmail_password'],
     authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 }
+    enable_starttls_auto: true}
 
 
   config.action_mailer.perform_deliveries = true
